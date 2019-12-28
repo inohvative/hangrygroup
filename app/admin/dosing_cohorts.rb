5975dda28c9d9fc9681499efe4dd810ef35b20ab
Raghu Betina <raghu@firstdraft.com>
2019-12-28 01:42:58 +0000

    Whitelist columns in DosingCohort for ActiveAdmin

--- app/admin/dosing_cohorts.rb
+++ app/admin/dosing_cohorts.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register DosingCohort do
+
+ permit_params :cohort_123
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
