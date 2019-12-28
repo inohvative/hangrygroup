d16717bf727433db06f05f0b695116d5e1777993
Raghu Betina <raghu@firstdraft.com>
2019-12-28 01:43:12 +0000

    Whitelist columns in InsulinBolu for ActiveAdmin

--- app/admin/insulin_bolus.rb
+++ app/admin/insulin_bolus.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register InsulinBolu do
+
+ permit_params :name, :amount, :subject_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
