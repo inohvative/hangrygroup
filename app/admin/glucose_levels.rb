a32f48cc02704789954713e8051a5f1ffc16e11d
Raghu Betina <raghu@firstdraft.com>
2019-12-28 01:43:08 +0000

    Whitelist columns in GlucoseLevel for ActiveAdmin

--- app/admin/glucose_levels.rb
+++ app/admin/glucose_levels.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register GlucoseLevel do
+
+ permit_params :glucose_level, :subject_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
