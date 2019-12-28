a173ce8194990c096f7037c8ebce35a74b045570
Raghu Betina <raghu@firstdraft.com>
2019-12-28 01:43:02 +0000

    Whitelist columns in Subject for ActiveAdmin

--- app/admin/subjects.rb
+++ app/admin/subjects.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Subject do
+
+ permit_params :cohort_id, :profilenotes
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
